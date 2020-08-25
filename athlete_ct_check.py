255ea36c240d683b9b6cffb6961072f90ad48ebc
Ido Gross <53366863+idogross@users.noreply.github.com>
2020-08-25 10:16:39 +0300

    few corrections

--- athlete_ct_check.py
+++ athlete_ct_check.py
@@ -4,11 +4,10 @@ import cv2
 
 # read image
 image = cv2.imread('Binary.bmp', 0)
-mask = np.zeros(image.shape)
 
 # Opening to clean image
 se_circ = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (26, 26))
-opened = cv2.morphologyEx(src=image, dst=mask, kernel=se_circ, op=cv2.MORPH_OPEN)
+opened = cv2.morphologyEx(src=image, kernel=se_circ, op=cv2.MORPH_OPEN)
 
 # show opened image
 cv2.imshow('Opening', opened)
@@ -27,18 +26,16 @@ cv2.imshow(f'Area of liver in pixels: {str(int(area))}', mask_liver)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Liver bounadries
-mask2 = np.zeros(image.shape)
+# Liver boundaries
 SE_circ2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-eroded = cv2.morphologyEx(src=mask_liver, dst=mask2, kernel=SE_circ2, op=cv2.MORPH_ERODE)
+eroded = cv2.morphologyEx(src=mask_liver, kernel=SE_circ2, op=cv2.MORPH_ERODE)
 Liver_boundary = mask_liver - eroded
 
-# Originial liver ct + boundary
-Liver_CT = cv2.imread('Liver_CT.bmp', 0)
-Liver_CT_combined = np.zeros(Liver_CT.shape)
-Liver_CT_combined = cv2.addWeighted(Liver_CT, 0.8, Liver_boundary, 0.5, gamma=0)
+# Original liver ct + boundary
+liver_CT = cv2.imread('Liver_CT.bmp', 0)
+liver_CT_combined = cv2.addWeighted(liver_CT, 0.6, Liver_boundary, 0.4, gamma=0)
 
 # Show liver and boundary
-cv2.imshow('Liver and Boundary', Liver_CT_combined)
+cv2.imshow('Liver and Boundary', liver_CT_combined)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
